bfecbeaab4ab1cfdf0269a9ff1c9da5ea6b3a37a
SteTala97 <stefano.talamona.97@gmail.com>
2022-06-11 10:01:55 +0200

    Initial commit

--- server/config/gamesPerDateConfig.js
+++
@@ -1,8 +0,0 @@
-// let per i test, in produzione andrà messo const
-let apiHeader = {
-    'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': '1f6dbbda61msh703464de9effb2dp15b435jsn1f7c922e140c',
-    'Content-Type': 'application/json'
-};
-
-exports.apiHeader = apiHeader;
